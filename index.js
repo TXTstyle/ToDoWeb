20b9c10989007554750f49c0f75c0b9e45d71e57
Oskar Engström <engstrom.oskar@elev.ga.ntig.se>
2022-01-13 13:58:09 +0100

    HTML/css

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = 'localhost';
+const port = 5000;
 
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'));
 
 app.get('/', (req,res) => {
     res.render('./index')
 });
 
 app.listen(port, () => {
-    console.log('Running on http://localhost:3000/')
+    console.log('Running')
 });
